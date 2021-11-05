37aef1d82b7d00579d7a316e458b6c2be87fe505
WhiteBlackGoose <wbg@angouri.org>
2021-11-05 15:29:17 +0300

    Mem alignment benchmarks

---
+++ Benchmarks/MemoryAlignment/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+var b = new AddArrays();
+
+b.ElemCount = 0x10;
+b.Offset = 0;
+b.Setup();
+Console.WriteLine(b.NaiveAdd());
+Console.WriteLine(b.SimdAdd());
+
+b.ElemCount = 0x10;
+b.Offset = 46;
+b.Setup();
+Console.WriteLine(b.NaiveAdd());
+Console.WriteLine(b.SimdAdd());
+*/
+
+using BenchmarkDotNet.Running;
+
+BenchmarkRunner.Run<AddArrays>();
